0ae1973b1b4b41f7ffe960293a852a9ab6d96929
OW-F-Beytell <125928514+OW-F-Beytell@users.noreply.github.com>
2023-07-31 23:46:45 +0200

    Init

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $("#test").click(function (){
+
+        $("h1").toggle();
+        console.log("Hello Im doing it");
+        alert("Hello World!");
+
+    });
+    
+   
+
+});
